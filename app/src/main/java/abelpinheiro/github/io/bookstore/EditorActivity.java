5f9055cc151bc5853fe40b0fd10c1a1fc20d530a
abel <abelpinheiro95@gmail.com>
2018-09-17 00:45:10 -0300

    mudado botão de ADD informações iniciais da tela de EditorActivity

--- app/src/main/java/abelpinheiro/github/io/bookstore/EditorActivity.java
+++ app/src/main/java/abelpinheiro/github/io/bookstore/EditorActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 
 public class EditorActivity extends AppCompatActivity {
 
+    private static final String SAVE_ACTIVITY_TITLE = "SAVE_ACTIVITY_TITLE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
-        String title = getIntent().getStringExtra("SAVE_ACTIVITY_TITLE");
+        String title = getIntent().getStringExtra(SAVE_ACTIVITY_TITLE);
         setTitle(title);
     }
 }
